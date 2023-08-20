cd164636b5f460848a0096b83e3269c0d1299f88
evanpurl <evan@purlcomputers.com>
2023-08-20 10:23:32 -0600

    Added install_requirements section to main.py. upgrades pip and installs from requirements.txt if it exists.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import argparse
 import asyncio
 import os
+import subprocess
+import sys
 
 import discord
 from util.load_extensions import load_extensions  # Our code
@@ -14,14 +17,23 @@ client = commands.Bot(command_prefix="$", intents=intents)
 
 load_dotenv()
 
+parser = argparse.ArgumentParser(description='Nite Life Network Bot')
+
+
+async def install_requirements():
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', 'pip'])
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r', 'requirements.txt'])
+
 
 # Main function to load extensions and then load bot.
 async def main():
+    await install_requirements()
     async with client:
         try:
             token = os.getenv('token')
 
             await load_extensions(client)
+            print("Bot Ready!")
             await client.start(token)
         except KeyboardInterrupt:
             pass
