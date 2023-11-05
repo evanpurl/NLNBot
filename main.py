41d01eedd60008d91b5f46676de094eb791bc6f9
evanpurl <evan@purlcomputers.com>
2023-11-05 08:12:25 -0700

    Improved main.py

--- main.py
+++ main.py
@@ -1,18 +1,5 @@
 import os
-import subprocess
-import sys
-
-
-def install_requirements():
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', 'pip'])
-    if os.path.exists('requirements.txt'):
-        subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r', 'requirements.txt'])
-
-
-install_requirements()
-
 import asyncio
-
 import discord
 from util.load_extensions import load_extensions  # Our code
 from discord.ext import commands
