3e3a387c2b94f3721da365d7ea8c6f7e487711ef
evanpurl <evan@purlcomputers.com>
2024-02-21 16:48:56 -0700

    Removed features

--- cogs/config.py
+++ cogs/config.py
@@ -11,31 +11,31 @@ class setcmd(commands.GroupCog, name="set"):
     def __init__(self, bot):
         self.bot = bot
 
-    @app_commands.checks.has_permissions(manage_channels=True)
-    @app_commands.command(name="welcome-channel", description="Command to set your server's welcome channel.")
-    async def welcomechannel(self, interaction: discord.Interaction, channel: discord.TextChannel):
-        try:
-
-            await insert(self.bot.database, f"""REPLACE INTO server_{str(interaction.guild.id)} (configname, configoption) VALUES( 'welcome_channel', {channel.id})""")
-            await interaction.response.send_message(
-                f"Welcome Channel has been set to {discord.utils.get(interaction.guild.channels, id=channel.id).mention}.",
-                ephemeral=True)
-        except Error as e:
-            print(e)
-            await interaction.response.send_message(content=f"""Something went wrong.""", ephemeral=True)
-
-    @app_commands.checks.has_permissions(manage_channels=True)
-    @app_commands.command(name="goodbye-channel", description="Command to set your server's goodbye channel.")
-    async def goodbyechannel(self, interaction: discord.Interaction, channel: discord.TextChannel):
-        try:
-            await insert(self.bot.database,
-                         f"""REPLACE INTO server_{str(interaction.guild.id)} (configname, configoption) VALUES( 'goodbye_channel', {channel.id})""")
-            await interaction.response.send_message(
-                f"Goodbye Channel has been set to {discord.utils.get(interaction.guild.channels, id=channel.id)}.",
-                ephemeral=True)
-        except Exception as e:
-            print(e)
-            await interaction.response.send_message(content=f"""Something went wrong.""", ephemeral=True)
+    # @app_commands.checks.has_permissions(manage_channels=True)
+    # @app_commands.command(name="welcome-channel", description="Command to set your server's welcome channel.")
+    # async def welcomechannel(self, interaction: discord.Interaction, channel: discord.TextChannel):
+    #     try:
+    #
+    #         await insert(self.bot.database, f"""REPLACE INTO server_{str(interaction.guild.id)} (configname, configoption) VALUES( 'welcome_channel', {channel.id})""")
+    #         await interaction.response.send_message(
+    #             f"Welcome Channel has been set to {discord.utils.get(interaction.guild.channels, id=channel.id).mention}.",
+    #             ephemeral=True)
+    #     except Error as e:
+    #         print(e)
+    #         await interaction.response.send_message(content=f"""Something went wrong.""", ephemeral=True)
+    #
+    # @app_commands.checks.has_permissions(manage_channels=True)
+    # @app_commands.command(name="goodbye-channel", description="Command to set your server's goodbye channel.")
+    # async def goodbyechannel(self, interaction: discord.Interaction, channel: discord.TextChannel):
+    #     try:
+    #         await insert(self.bot.database,
+    #                      f"""REPLACE INTO server_{str(interaction.guild.id)} (configname, configoption) VALUES( 'goodbye_channel', {channel.id})""")
+    #         await interaction.response.send_message(
+    #             f"Goodbye Channel has been set to {discord.utils.get(interaction.guild.channels, id=channel.id)}.",
+    #             ephemeral=True)
+    #     except Exception as e:
+    #         print(e)
+    #         await interaction.response.send_message(content=f"""Something went wrong.""", ephemeral=True)
 
     @app_commands.checks.has_permissions(manage_channels=True)
     @app_commands.command(name="ticket-category", description="Command to set your server's ticket category.")
@@ -64,17 +64,17 @@ class setcmd(commands.GroupCog, name="set"):
             print(e)
             await interaction.response.send_message(content=f"""Something went wrong.""", ephemeral=True)
 
-    @app_commands.checks.has_permissions(manage_roles=True)
-    @app_commands.command(name="default-role", description="Command for setting your server's Default role.")
-    async def defaultrole(self, interaction: discord.Interaction, role: discord.Role):
-        try:
-            await insert(self.bot.database,
-                         f"""REPLACE INTO server_{str(interaction.guild.id)} (configname, configoption) VALUES( 'default_role', {role.id})""")
-            await interaction.response.send_message(
-                content=f"""Default Role has been set to {role.name}""", ephemeral=True)
-        except Exception as e:
-            print(e)
-            await interaction.response.send_message(content=f"""Something went wrong.""", ephemeral=True)
+    # @app_commands.checks.has_permissions(manage_roles=True)
+    # @app_commands.command(name="default-role", description="Command for setting your server's Default role.")
+    # async def defaultrole(self, interaction: discord.Interaction, role: discord.Role):
+    #     try:
+    #         await insert(self.bot.database,
+    #                      f"""REPLACE INTO server_{str(interaction.guild.id)} (configname, configoption) VALUES( 'default_role', {role.id})""")
+    #         await interaction.response.send_message(
+    #             content=f"""Default Role has been set to {role.name}""", ephemeral=True)
+    #     except Exception as e:
+    #         print(e)
+    #         await interaction.response.send_message(content=f"""Something went wrong.""", ephemeral=True)
 
     @app_commands.checks.has_permissions(manage_roles=True)
     @app_commands.command(name="supporter-role", description="Command for setting your server's supporter role.")
@@ -88,9 +88,9 @@ class setcmd(commands.GroupCog, name="set"):
             print(e)
             await interaction.response.send_message(content=f"""Something went wrong.""", ephemeral=True)
 
-    @welcomechannel.error
+    #@welcomechannel.error
     @transcriptlogchannel.error
-    @defaultrole.error
+    #@defaultrole.error
     @ticketcategory.error
     async def onerror(self, interaction: discord.Interaction, error: app_commands.MissingPermissions):
         await interaction.response.send_message(content=error,
@@ -102,31 +102,31 @@ class resetcmd(commands.GroupCog, name="reset"):
     def __init__(self, bot):
         self.bot = bot
 
-    @app_commands.checks.has_permissions(manage_channels=True)
-    @app_commands.command(name="welcome-channel", description="Command to reset your server's welcome channel.")
-    async def welcomechannel(self, interaction: discord.Interaction):
-        try:
-            await insert(self.bot.database,
-                         f"""REPLACE INTO server_{str(interaction.guild.id)} (configname, configoption) VALUES( 'welcome_channel', 0)""")
-            await interaction.response.send_message(
-                f"Welcome Channel has been reset.",
-                ephemeral=True)
-        except Exception as e:
-            print(e)
-            await interaction.response.send_message(content=f"""Something went wrong.""", ephemeral=True)
+    # @app_commands.checks.has_permissions(manage_channels=True)
+    # @app_commands.command(name="welcome-channel", description="Command to reset your server's welcome channel.")
+    # async def welcomechannel(self, interaction: discord.Interaction):
+    #     try:
+    #         await insert(self.bot.database,
+    #                      f"""REPLACE INTO server_{str(interaction.guild.id)} (configname, configoption) VALUES( 'welcome_channel', 0)""")
+    #         await interaction.response.send_message(
+    #             f"Welcome Channel has been reset.",
+    #             ephemeral=True)
+    #     except Exception as e:
+    #         print(e)
+    #         await interaction.response.send_message(content=f"""Something went wrong.""", ephemeral=True)
 
-    @app_commands.checks.has_permissions(manage_channels=True)
-    @app_commands.command(name="goodbye-channel", description="Command to reset your server's goodbye channel.")
-    async def goodbyechannel(self, interaction: discord.Interaction):
-        try:
-            await insert(self.bot.database,
-                         f"""REPLACE INTO server_{str(interaction.guild.id)} (configname, configoption) VALUES( 'goodbye_channel', 0)""")
-            await interaction.response.send_message(
-                f"Goodbye Channel has been reset.",
-                ephemeral=True)
-        except Exception as e:
-            print(e)
-            await interaction.response.send_message(content=f"""Something went wrong.""", ephemeral=True)
+    # @app_commands.checks.has_permissions(manage_channels=True)
+    # @app_commands.command(name="goodbye-channel", description="Command to reset your server's goodbye channel.")
+    # async def goodbyechannel(self, interaction: discord.Interaction):
+    #     try:
+    #         await insert(self.bot.database,
+    #                      f"""REPLACE INTO server_{str(interaction.guild.id)} (configname, configoption) VALUES( 'goodbye_channel', 0)""")
+    #         await interaction.response.send_message(
+    #             f"Goodbye Channel has been reset.",
+    #             ephemeral=True)
+    #     except Exception as e:
+    #         print(e)
+    #         await interaction.response.send_message(content=f"""Something went wrong.""", ephemeral=True)
 
     @app_commands.checks.has_permissions(manage_channels=True)
     @app_commands.command(name="ticket-category", description="Command to reset your server's ticket category.")
@@ -155,17 +155,17 @@ class resetcmd(commands.GroupCog, name="reset"):
             print(e)
             await interaction.response.send_message(content=f"""Something went wrong.""", ephemeral=True)
 
-    @app_commands.checks.has_permissions(manage_roles=True)
-    @app_commands.command(name="default-role", description="Command to reset your server's Default role.")
-    async def defaultrole(self, interaction: discord.Interaction):
-        try:
-            await insert(self.bot.database,
-                         f"""REPLACE INTO server_{str(interaction.guild.id)} (configname, configoption) VALUES( 'default_role', 0)""")
-            await interaction.response.send_message(
-                content=f"""Default Role has been reset.""", ephemeral=True)
-        except Exception as e:
-            print(e)
-            await interaction.response.send_message(content=f"""Something went wrong.""", ephemeral=True)
+    # @app_commands.checks.has_permissions(manage_roles=True)
+    # @app_commands.command(name="default-role", description="Command to reset your server's Default role.")
+    # async def defaultrole(self, interaction: discord.Interaction):
+    #     try:
+    #         await insert(self.bot.database,
+    #                      f"""REPLACE INTO server_{str(interaction.guild.id)} (configname, configoption) VALUES( 'default_role', 0)""")
+    #         await interaction.response.send_message(
+    #             content=f"""Default Role has been reset.""", ephemeral=True)
+    #     except Exception as e:
+    #         print(e)
+    #         await interaction.response.send_message(content=f"""Something went wrong.""", ephemeral=True)
 
     @app_commands.checks.has_permissions(manage_roles=True)
     @app_commands.command(name="supporter-role", description="Command to reset your server's supporter role.")
@@ -179,9 +179,9 @@ class resetcmd(commands.GroupCog, name="reset"):
             print(e)
             await interaction.response.send_message(content=f"""Something went wrong.""", ephemeral=True)
 
-    @welcomechannel.error
+    #@welcomechannel.error
     @transcriptlogchannel.error
-    @defaultrole.error
+    #@defaultrole.error
     @ticketcategory.error
     async def onerror(self, interaction: discord.Interaction, error: app_commands.MissingPermissions):
         await interaction.response.send_message(content=error,
